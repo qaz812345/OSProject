6827d8e305a1f02792613e28f66f60fe659055fb
qaz812345 <qaz812345@gmail.com>
2018-11-01 02:10:53 +0800

    Update TobaccoSmokers and is runnable

--- TobaccoSmokers/src/Main.java
+++ TobaccoSmokers/src/Main.java
@@ -1,25 +1,28 @@
-package application;
-	
+
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
+import javafx.scene.Parent;
 
 public class Main extends Application {
+
 	@Override
 	public void start(Stage primaryStage) {
+		Parent root;
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			root = FXMLLoader.load(getClass().getResource("IndexPage.fxml"));
+			Scene scenc = new Scene(root);
+			primaryStage.setScene(scenc);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
