c7f0e0c83c1381b4595ac6d55feaa101b5a51e1d
qaz812345 <qaz812345@gmail.com>
2018-12-25 23:34:15 +0800

    Add music and remove unuse code

--- TobaccoSmokers/src/Agent.java
+++ TobaccoSmokers/src/Agent.java
@@ -1,21 +1,13 @@
 import java.lang.Thread;
 
-import javafx.scene.image.ImageView;
-
 public class Agent implements Runnable{
 	
 	private Table table;
 	private int item;
-	public ImageView img;
-	public int xOffset;
-	public int yOffset;
 	
-	public Agent(Table t,int id,ImageView i,int xo,int yo) {
+	public Agent(Table t,int id) {
 		table=t;
 		item=id;
-		img=i;
-		xOffset=xo;
-		yOffset=yo;
 	}
 	
 	@Override
@@ -23,11 +15,10 @@ public class Agent implements Runnable{
 		// TODO Auto-generated method stub
 		try {
 			Thread.sleep((3-item)*100);
-			//img.setVisible(false);
 			while(true) {
 				int s=table.getPoissonRandom(5);
 				if(Table.setedItem[item]==false && Table.itemCount<2){
-					table.setTable(item,s);		
+					Table.setTable(item,s);		
 				}else {
 					Thread.sleep(s*1000);
 				}		
--- TobaccoSmokers/src/Smoker.java
+++ TobaccoSmokers/src/Smoker.java
@@ -4,7 +4,6 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Pos;
 import javafx.util.Duration;
 import javafx.application.Platform;
 import javafx.scene.control.Label;
@@ -16,15 +15,15 @@ public class Smoker implements Runnable{
 	private Table table;
 	private int item;
 	private int smokeCounter;
+	private int xOffset;
+	private int yOffset;
+	private int i=0;
+	private String name;
+	private Label countLab;
 	public ImageView img;
 	final public ArrayList<Image> imgList;
-	public String name;
-	public Label countLab;
-	public int xOffset;
-	public int yOffset;
-	public int i=0;
 	
-	public Smoker(Table t,int id,ImageView i,String s,Label c,int x,int y) {
+	public Smoker(Table t,int id,ImageView i,String s,Label c,int xo,int yo) {
 		table=t;
 		item=id;
 		smokeCounter=0;
@@ -39,15 +38,15 @@ public class Smoker implements Runnable{
 		imgList.add(new Image("file:res/drawable/"+name+"2.png"));
 		imgList.add(new Image("file:res/drawable/"+name+"1.png"));
 		imgList.add(new Image("file:res/drawable/"+name+"0.png"));
-		xOffset=x;
-		yOffset=y;
+		xOffset=xo;
+		yOffset=yo;
 	}
 	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		try {
-			Thread.sleep((3-item)*100);
+			//Thread.sleep((3-item)*100);
 			while(true) {
 				if(Table.turn==item) {
 					synchronized(table) {						
@@ -70,13 +69,13 @@ public class Smoker implements Runnable{
 						    timeline.setCycleCount(7);
 						    timeline.playFromStart();
 						});
-						table.setTimeLabel(Table.sleepTime);
-						Thread.sleep(Table.sleepTime*1000);
+						Table.setTimeLabel(Table.smokeTime);
+						Thread.sleep(Table.smokeTime*1000);
 						System.out.println("Smoker with "+table.getItem(item)+" is smoking...");
 						counterIncrease();
 						Platform.runLater(()->countLab.setText(String.valueOf(smokeCounter)));
 						System.out.println("Smoker with "+table.getItem(item)+" smokes "+smokeCounter+" time.");
-						table.tidyTable();
+						Table.tidyTable();
 					}
 				}else {
 					Thread.sleep(100);
